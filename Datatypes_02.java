198260fd2144aa9709c1a38cbca46ad01e22feb1
Leela <leelanarayan1214@gmail.com>
2026-03-18 08:56:58 +0530

    Fixing Data types issues

--- Datatypes_02.java
+++ Datatypes_02.java
@@ -1,18 +1,24 @@
 public class Datatypes_02 {
    	public static void main(String[] args)
      {
-        
+        System.out.println("Datatypes in Java");
         int n = 10;
+        System.out.println("Integer: " + n);
 		byte by=127;
+        System.out.println("Byte: " + by);
 		short sh=558;
+        System.out.println("Short: " + sh);
 		long l=5854l;
-		
+		System.out.println("Long: " + l);
 		float f=5.8f;
 		double d=5.8;
-		
+		System.out.println("Float: " + f);
+		System.out.println("Double: " + d);
 		char c='k';
+        System.out.println("Char: " + c);
 		
 		boolean b=true;
+        System.out.println("Boolean: " + b);
 
 	}
 

0e756fe791fb009b2109f1b505e461184d4f1d90
Leela <leelanarayan1214@gmail.com>
2026-04-01 07:46:42 +0530

    Concept of string pool

--- String_23.java
+++ String_23.java
@@ -6,5 +6,16 @@ public class String_23 {
 		System.out.println(name.hashCode());
 		System.out.println("hello "+name);
 		System.out.println(name.concat("reddy"));
+
+    // String pool
+    String Name="navin";
+		Name=Name+"reddy";
+		System.out.println("hello"+Name);
+		
+		String s1="Navin";
+		String s2="Navin";
+		
+		System.out.println(s1==s2);	
+		
    }
 }

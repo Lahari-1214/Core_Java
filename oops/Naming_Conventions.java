4a75cb081c9ae1e41084d3f707e8ea64906e8aa3
Leela <leelanarayan1214@gmail.com>
2026-04-06 23:26:04 +0530

    How to name a variable

---
+++ oops/Naming_Conventions.java
@@ -0,0 +1,24 @@
+public class Naming_Conventions
+{
+	public static void main(String a[]) 
+	{
+	int age=20;
+	final double PI=3.14;
+	String name="John";
+	}
+}
+
+
+// Camel casing
+
+// class and interface - Calc       ( first letter capital)
+//variable and method - marks, show()    (small letters)
+// constants - PIE, BRAND       (all capital letters)
+
+
+//showMyMarks()     (first letter of each word is capital from second word)
+//show_my_marks()   (words join by underscore)
+
+// MyData
+
+// age, DATA, Human()
